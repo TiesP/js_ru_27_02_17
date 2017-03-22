65b99efcfd1761a8483f582b3c42e9f9f2bb28ea
Roman <kaboom.ua@gmail.com>
2017-03-22 23:09:57 +0200

    Update newCommentId.js

--- src/middlewares/newCommentId.js
+++ src/middlewares/newCommentId.js
@@ -3,6 +3,7 @@ import {ADD_COMMENT} from '../constants'
 export default store => next => action => {
     const { type } = action
     switch (type) {
+    //через мидлвары будет проходить каждый экшин, они должны быть максимально общими, завязывать на конкретные экшины - плохая практика
         case ADD_COMMENT:
             next({...action, id: newId(store.getState())})
      }
